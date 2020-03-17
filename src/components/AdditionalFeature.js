54291ad3d4d78e2724b5a024d885c3261131103d
biskoi <biskoi@daydream.cafe>
2020-03-17 15:57:40 -0600

    project mvp

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {removeFeature, addFeature} from '../actions/actions';
 
 const AdditionalFeature = props => {
+
+  const addfeature = (feature) => {
+    props.addFeature(feature)
+  }
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick = {() => addfeature(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, {addFeature})(AdditionalFeature)
